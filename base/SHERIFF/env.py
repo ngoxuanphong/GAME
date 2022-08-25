6b4c15342815ccd3ad9d33b4ae6d3439bdbb6149
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-08-25 15:13:12 +0700

    add sheriff

--- base/SHERIFF/env.py
+++ base/SHERIFF/env.py
@@ -91,7 +91,13 @@ def get_list_action(player_state):
 
     elif phase_env == 2:
         #lấy thẻ từ các chồng bài: 16-bài rút, 17-lật trái, 18-lật phải
-        return np.array([16, 17, 18])    
+        list_action = np.array([16])
+        all_left_right_up = player_state[171:201]
+        if np.sum(all_left_right_up[:15]) > 0:
+            list_action = np.append(list_action, 17)
+        if np.sum(all_left_right_up[15:]) > 0:
+            list_action = np.append(list_action, 18)
+        return list_action
 
     elif phase_env == 3:
         #trả thẻ bỏ vào  chồng bài lật: 19: lật trái, 20: lật phải
@@ -427,8 +433,12 @@ def step(env_state, action, all_penalty):
     return env_state
 
 @njit(fastmath=True, cache=True)
-def amount_action_space():
+def amount_action():
     return 83
+
+@njit(fastmath=True, cache=True)
+def amount_player():
+    return 4
 
 @njit(fastmath=True, cache=True)  
 def system_check_end(env_state):
@@ -469,7 +479,7 @@ def check_winner(env_state):
         top1 = np.max(count_type)
         if top1 == 0:
             continue
-        count_top1 = len(np.where(count_type == top1))
+        count_top1 = len(np.where(count_type == top1)[0])
         if count_top1 > 1:
             reward_i = np.floor((reward_King[type_i] + reward_Queen[type_i])/count_top1)
             all_player_coin = np.where(count_type == top1, all_player_coin + reward_i, all_player_coin)
@@ -481,7 +491,7 @@ def check_winner(env_state):
             top2 = np.max(count_type)
             if top2 == 0:
                 continue
-            count_top2 = len(np.where(count_type == top2))
+            count_top2 = len(np.where(count_type == top2)[0])
             if count_top2 > 1:
                 reward_i = np.floor(reward_Queen[type_i]/count_top2)
                 all_player_coin = np.where(count_type == top2, all_player_coin + reward_i, all_player_coin)
@@ -494,25 +504,25 @@ def check_winner(env_state):
 
     all_player_coin_int = np.array([int(all_player_coin[i]) for i in range(len(all_player_coin))])
     temp_win = np.max(all_player_coin_int)
-    number_win = np.where(all_player_coin_int == temp_win)
-    if len(number_win[0]) == 1:
-        return number_win[0][0]
+    number_win = np.where(all_player_coin_int == temp_win)[0]
+    if len(number_win) == 1:
+        return number_win[0]
     else:
         all_legal = np.zeros(4)
-        for id in number_win[0]:
+        for id in number_win:
             all_legal[id] = np.sum(all_done_card[15*id:15*(id+1)][:4])
         temp_win = np.max(all_legal)
-        number_win = np.where(all_legal == temp_win)
-        if len(number_win[0]) == 1:
-            return number_win[0][0]
+        number_win = np.where(all_legal == temp_win)[0]
+        if len(number_win) == 1:
+            return number_win[0]
         else:
             all_unlegal = np.zeros(4)
-            for id in number_win[0]:
+            for id in number_win:
                 all_unlegal[id] = np.sum(all_done_card[15*id:15*(id+1)][4:])
             temp_win = np.max(all_unlegal)
-            number_win = np.where(all_unlegal == temp_win)
-            if len(number_win[0]) == 1:
-                return number_win[0][0]
+            number_win = np.where(all_unlegal == temp_win)[0]
+            if len(number_win) == 1:
+                return number_win[0]
             else:
                 all_player_coin_last = np.zeros(4)
                 all_player_coin_last[number_win] = all_player_coin[number_win]
@@ -559,60 +569,346 @@ def check_victory(player_state):
                         return 1
                     else:
                         return 0
-               
+
+@njit(fastmath=True, cache=True)
+def step_print_mode(env_state, action, all_penalty):
+    phase_env = env_state[-1]
+    id_action = int(env_state[-3])
+    player_in4 = env_state[96*id_action:96*(id_action+1)]
+    # print(player_in4)
+
+    if phase_env == 1:
+        if action != 15:
+            player_card = player_in4[-15:]
+            temp_drop_card = env_state[754:769]
+            player_card[action] -= 1
+            temp_drop_card[action] += 1
+            player_in4[-15:] = player_card
+            env_state[96*id_action:96*(id_action+1)] = player_in4
+            env_state[754:769] = temp_drop_card
+            if np.sum(player_card) == 0:
+                env_state[-1] += 1
+        else:
+            env_state[-1] += 1
+
+    elif phase_env == 2: 
+        if action == 16:
+            player_card = player_in4[-15:]
+            # print('check', player_card)
+            card_down = env_state[384:504]
+            number_card_get = int(6 - np.sum(player_card))
+            card_get = card_down[:number_card_get]
+            print('lấy thẻ chồng bài úp: ', card_get)
+            if len(card_get) > 0:
+                for card in card_get:
+                    player_card[int(card)-1] += 1
+            card_down = np.concatenate((card_down[number_card_get:], np.zeros(number_card_get)))
+            env_state[384:504] = card_down
+            player_in4[-15:] = player_card
+            env_state[96*id_action:96*(id_action+1)] = player_in4
+            if np.sum(env_state[754:769]) > 0:
+                env_state[-1] += 1
+            else:
+                env_state[-1] = 1
+                env_state[-3] = (env_state[-3] + 1)%4
+                if env_state[-3] == (env_state[-2] - 1)%4:
+                    env_state[-3] = (env_state[-3] + 1)%4
+                    env_state[-1] = 4
+
+        else: 
+            player_card = player_in4[-15:]
+            card_up = env_state[504+125*(action-17):504+125*(action-16)]
+            card_get = card_up[0]
+            print('lấy thẻ chồng bài lật: ', card_get)
+            player_card[int(card_get)-1] += 1
+            card_up = np.append(card_up[1:], 0)
+            env_state[504+125*(action-17):504+125*(action-16)] = card_up
+            player_in4[-15:] = player_card
+            env_state[96*id_action:96*(id_action+1)] = player_in4
+            if np.sum(player_card) == 6:
+                if np.sum(env_state[754:769]) > 0:
+                    env_state[-1] += 1
+                else:
+                    env_state[-1] = 1
+                    env_state[-3] = (env_state[-3] + 1)%4
+                    if env_state[-3] == (env_state[-2] - 1)%4:
+                        env_state[-3] = (env_state[-3] + 1)%4
+                        env_state[-1] = 4
+
+    elif phase_env == 3:
+        temp_card_drop = env_state[754:769]
+        card_up = env_state[504+125*(action-19):504+125*(action-18)]
+        for i in range(len(temp_card_drop)):
+            if temp_card_drop[i] > 0:
+                card_up = np.append(np.array([i+1]*int(temp_card_drop[i])), card_up)
+        if np.sum(temp_card_drop) > 0:
+            card_up = card_up[:int(-np.sum(temp_card_drop))]
+        temp_card_drop = np.zeros(15)
+        env_state[754:769] = temp_card_drop
+        env_state[504+125*(action-19):504+125*(action-18)] = card_up
+        env_state[-1] = 1
+        env_state[-3] = (env_state[-3] + 1)%4
+        if env_state[-3] == (env_state[-2] - 1)%4:
+            env_state[-3] = (env_state[-3] + 1)%4
+            env_state[-1] = 4
+
+    elif phase_env == 4:
+        card_bag = player_in4[-45:-30]
+        player_card = player_in4[-15:]
+        if action == 36:
+            if np.sum(card_bag) == 0:
+                print('T TOANG NÀY')
+                return env_state
+            else:
+                env_state[-3] = (env_state[-3] + 1)%4
+                if env_state[-3] == (env_state[-2] - 1)%4:
+                    env_state[-3] = (env_state[-3] + 1)%4
+                    env_state[-1] +=1
+        else:
+            card_bag[action-21] += 1
+            player_card[action-21] -= 1
+            player_in4[-15:] = player_card
+            player_in4[-45:-30] = card_bag
+            env_state[96*id_action:96*(id_action+1)] = player_in4
+            if np.sum(card_bag) == 5:
+                env_state[-3] = (env_state[-3] + 1)%4
+                if env_state[-3] == (env_state[-2] - 1)%4:
+                    env_state[-3] = (env_state[-3] + 1)%4
+                    env_state[-1] +=1
+
+    elif phase_env == 5:
+        type_bag = action - 36
+        player_in4[2] = type_bag
+        env_state[96*id_action:96*(id_action+1)] = player_in4
+        env_state[-3] = (env_state[-3] + 1)%4
+        if env_state[-3] == (env_state[-2] - 1)%4:
+            env_state[-1] +=1
+
+    elif phase_env == 6:
+        player_checked = action-40 
+        env_state[-3] = (env_state[-3] + player_checked)%4
+        env_state[-4] += 1
+        env_state[-1] +=1
+        env_state[-5] = env_state[-3]
+
+    elif phase_env == 7:
+        if action == 46:
+            player_in4[0] -= 1
+            #new: chỉnh vị trí coin hối lộ
+            player_in4[3] += 1
+            env_state[96*id_action:96*(id_action+1)] = player_in4
+        else:
+            env_state[-1] += 1
+
+    elif phase_env == 8:
+        if action == 62:
+            env_state[-1] += 1
+        else:
+            all_card_done = player_in4[-75:-60]
+            all_card_bride = player_in4[-90:-75]
+            card_bride = action-47
+            all_card_done[card_bride] -= 1
+            all_card_bride[card_bride] += 1
+            player_in4[-75:-60] = all_card_done
+            player_in4[-90:-75] = all_card_bride
+            env_state[96*id_action:96*(id_action+1)] = player_in4
+            # env_state[-1] += 1
+
+    elif phase_env == 9:
+        if action == 78:
+            env_state[-1] += 1
+            env_state[-3] = (env_state[-2] - 1)%4
+        else:
+            # print(player_in4[-90:-60])
+            all_card_bag = player_in4[-45:-30]
+            all_card_bride_bag = player_in4[-60:-45]
+            card_bride = action-63
+            all_card_bag[card_bride] -= 1
+            all_card_bride_bag[card_bride] += 1
+            #cập nhập số thẻ trong túi dùng hối lộ
+            player_in4[5] += 1
+            player_in4[-45:-30] = all_card_bag
+            player_in4[-60:-45] = all_card_bride_bag
+            env_state[96*id_action:96*(id_action+1)] = player_in4
+            # env_state[-1] += 1
+            # env_state[-3] = (env_state[-2] - 1)%4
+
+    elif phase_env == 10:
+        id_checked = int(env_state[-5])
+        player_checked = env_state[96*id_checked:96*(id_checked+1)]
+        type_bag = player_checked[2]
+        if action == 79:
+            #có check
+            #B1: khôi phục coin
+            player_checked[0] += player_checked[3]
+            player_checked[3] = 0
+            #B3 khôi phục thẻ done
+            player_checked[-75:-60] = player_checked[-75:-60] + player_checked[-90:-75]
+            player_checked[-90:-75] = np.zeros(15)
+            #khôi phục thẻ trong túi
+            player_checked[-45:-30] = player_checked[-45:-30] + player_checked[-60:-45]
+            player_checked[-60:-45] = np.zeros(15)
+            #tính toán cho sheriff
+            player_checked_card_bag = player_checked[-45:-30]
+            player_checked_card_done = player_checked[-75:-60]
+            card_bag_drop = player_in4[-30:-15]
+            #cập nhật coin
+            if player_checked_card_bag[int(type_bag)-1] == np.sum(player_checked_card_bag):
+                penalty = -player_checked_card_bag[int(type_bag)-1]*all_penalty[int(type_bag)-1]
+            else:
+                penalty = np.sum(np.multiply(player_checked_card_bag, all_penalty)) - player_checked_card_bag[int(type_bag)-1]*all_penalty[int(type_bag)-1]
+            player_in4[0] += penalty
+            player_checked[0] -= penalty
+            #cập nhật thẻ
+            for id in range(15):
+                if id == type_bag - 1:
+                    player_checked_card_done[id] += player_checked_card_bag[id]
+                else:
+                    card_bag_drop[id] += player_checked_card_bag[id]
+            player_checked[-45:-30] = np.zeros(15)
+            player_checked[-75:-60] = player_checked_card_done
+            player_checked[5] = 0
+            player_checked[2] = 0
+            player_checked[4] = np.sum(player_checked_card_done[4:])
+            env_state[96*id_checked:96*(id_checked+1)] = player_checked
+            player_in4[-30:-15] = card_bag_drop
+            env_state[96*id_action:96*(id_action+1)] = player_in4
+            #cập nhật trong hệ thống
+            if np.sum(card_bag_drop) > 0:
+                env_state[-1] += 1
+                env_state[-3] = (env_state[-2] - 1)%4
+            else:
+                if env_state[-4] == 3:
+                    env_state[-1] = 1
+                    env_state[-3] = (env_state[-2])%4
+                    env_state[-2] += 1
+                    if env_state[-3] == (env_state[-2]-1)%4:
+                        env_state[-3] = (env_state[-3] + 1)%4
+                    env_state[-4] = 0
+                else:
+                    env_state[-1] = 6
+                    env_state[-3] = (env_state[-2] - 1)%4
+        else:
+            #không check
+            #B1: Cập nhật coin
+            player_in4[0] += player_checked[3]
+            # player_checked[0] -= player_checked[3]
+            player_checked[3] = 0
+            #B3: cập nhật thẻ done sheriff and player_checked
+            # player_checked[-75:-60] = player_checked[-75:-60] - player_checked[-90:-75]
+            player_in4[-75:-60] = player_in4[-75:-60] + player_checked[-90:-75]
+            player_checked[-90:-75] = np.zeros(15)
+            #cập nhật thẻ done từ hối lộ thẻ trong túi và cập nhật thẻ trong túi
+            # player_checked[-45:-30] = player_checked[-45:-30] - player_checked[-60:-45]     #trừ thẻ trong túi đi
+            player_in4[-75:-60] = player_in4[-75:-60] + player_checked[-60:-45]
+            player_checked[-60:-45] = np.zeros(15)
+            #cập nhật thẻ done player_checked
+            player_checked[-75:-60] = player_checked[-75:-60] + player_checked[-45:-30]
+            player_checked[-45:-30] = np.zeros(15)
+            #cập nhật full thông tin
+            player_checked[5] = 0
+            player_checked[2] = 0
+            player_checked[4] = np.sum(player_checked[-75:-60][4:])
+            player_in4[4] = np.sum(player_in4[-75:-60][4:])
+            env_state[96*id_checked:96*(id_checked+1)] = player_checked
+            env_state[96*id_action:96*(id_action+1)] = player_in4
+            #cập nhật hệ thống lượt chơi
+            if env_state[-4] == 3:
+                env_state[-1] = 1
+                env_state[-3] = (env_state[-2])%4
+                env_state[-2] += 1
+                if env_state[-3] == (env_state[-2]-1)%4:
+                    env_state[-3] = (env_state[-3] + 1)%4
+                env_state[-4] = 0
+            else:
+                env_state[-1] = 6
+                env_state[-3] = (env_state[-2] - 1)%4
+
+    elif phase_env == 11:
+        temp_card_drop = player_in4[-30:-15]
+        card_up = env_state[504+125*(action-81):504+125*(action-80)]
+        for i in range(len(temp_card_drop)):
+            if temp_card_drop[i] > 0:
+                card_up = np.append(np.array([i+1]*int(temp_card_drop[i])), card_up)
+        # if np.sum(temp_card_drop) > 0:
+        card_up = card_up[:int(-np.sum(temp_card_drop))]
+        temp_card_drop = np.zeros(15)
+        player_in4[-30:-15] = temp_card_drop
+        env_state[504+125*(action-81):504+125*(action-80)] = card_up
+        env_state[96*id_action:96*(id_action+1)] = player_in4
+
+        if env_state[-4] == 3:
+            env_state[-1] = 1
+            env_state[-3] = (env_state[-2])%4
+            env_state[-2] += 1
+            if env_state[-3] == (env_state[-2]-1)%4:
+                env_state[-3] = (env_state[-3] + 1)%4
+            env_state[-4] = 0
+        else:
+            env_state[-1] = 6
+            env_state[-3] = (env_state[-2] - 1)%4
+    # print(env_state[-4:])
+    return env_state
+
+def one_game_print_mode(list_player, file_temp, file_per, all_penalty):
+    env_state = reset()
+    count_turn = 0
+    while not system_check_end(env_state):
+        action, file_temp, file_per = action_player(env_state,list_player,file_temp,file_per)
+        print(f'Turn: {count_turn} player {env_state[-3]} {all_action_mean[action]} {env_state[96*int(env_state[-3]):96*int(env_state[-3]+1)]} và {[env_state[0], env_state[96], env_state[192],env_state[288]]}')
+        env_state = step_print_mode(env_state, action, all_penalty)
+        count_turn += 1
+    winner = check_winner(env_state)
+    for id_player in range(4):
+        id_action = env_state[-3]
+        action, file_temp, file_per = action_player(env_state,list_player,file_temp,file_per)
+        print(f'Turn: {count_turn} player {env_state[-3]} {all_action_mean[action]} {env_state[96*int(env_state[-3]):96*int(env_state[-3]+1)]} và {[env_state[0], env_state[96], env_state[192],env_state[288]]}')
+        env_state[-3] = (env_state[-3] + 1)%4
+    return winner, file_per
+
+def normal_main_print_mode(list_player, times, file_per):
+    count = np.zeros(len(list_player))
+    all_penalty = np.array([2, 2, 2, 2, 4, 4, 4, 4, 3, 4, 4, 4, 4, 5, 5])
+    all_id_player = np.arange(len(list_player))
+    for van in range(times):
+        shuffle = np.random.choice(all_id_player, 4, replace=False)
+        shuffle_player = [list_player[shuffle[0]], list_player[shuffle[1]], list_player[shuffle[2]], list_player[shuffle[3]]]
+        file_temp = [[0],[0],[0],[0]]
+        # try:
+        winner, file_per = one_game_print_mode(shuffle_player, file_temp, file_per, all_penalty)
+        count[shuffle[winner]] += 1
+    return count, file_per
+
 def one_game(list_player, file_temp, file_per, all_penalty):
     env_state = reset()
     while not system_check_end(env_state):
         # player_state = state_to_player(env_state)
         action, file_temp, file_per = action_player(env_state,list_player,file_temp,file_per)
         env_state = step(env_state, action, all_penalty)
+    
     winner = check_winner(env_state)
     for id_player in range(4):
         env_state[-1] = 1
         id_action = env_state[-3]
-        player_state = state_to_player(env_state)
         action, file_temp, file_per = action_player(env_state,list_player,file_temp,file_per)
         env_state[-3] = (env_state[-3] + 1)%4
+
     return winner, file_per
 
-def normal_main(list_player, times, print_mode):
+def normal_main(list_player, times, file_per):
     count = np.zeros(len(list_player))
-    file_per = [0]
     all_penalty = np.array([2, 2, 2, 2, 4, 4, 4, 4, 3, 4, 4, 4, 4, 5, 5])
-    # count_game = np.zeros(10)
     all_id_player = np.arange(len(list_player))
     for van in range(times):
-        # print(van)
         shuffle = np.random.choice(all_id_player, 4, replace=False)
-        # for id in shuffle:
-        #     count_game[id] += 1
         shuffle_player = [list_player[shuffle[0]], list_player[shuffle[1]], list_player[shuffle[2]], list_player[shuffle[3]]]
-        file_temp = [[],[],[],[]]
+        file_temp = [[0],[0],[0],[0]]
         # try:
         winner, file_per = one_game(shuffle_player, file_temp, file_per, all_penalty)
         count[shuffle[winner]] += 1
     return count, file_per
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+all_action_mean = list(pd.read_excel('base/SHERIFF/SHERIFF.xlsx')['Mean'])
+# list_player = [player_random]*4
+# count_all, file_per_all = normal_main(list_player, 1, [0])
