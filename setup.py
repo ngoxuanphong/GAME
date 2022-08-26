aec8557e54197dd22a316096d831f110260a1011
phong <xuanphongcht@gmail.com>
2022-08-26 17:27:56 +0700

    remove phong agent

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 """
  - players: list player muốn truyền vào
-    - ở chế độ train thì sẽ train đa luồng tương ứng số người chơi
-    - chế độ test nếu truyền vào không đủ người chơi thì sẽ tự động thêm random vào
+    - ở chế độ Train thì sẽ train đa luồng tương ứng số người chơi
+    - chế độ Test nếu truyền vào không đủ người chơi thì sẽ tự động thêm random vào
  - games_name = ['Splendor_OnlyPlayerView','CENTURY', 'MACHIKORO', 'SHERIFF', 'Splendor', 'TLMN', 'TLMN_v2', 'SushiGo-main']
  - game_name: Tên game, có thể lấy ở games_name
- - time_run_game = Thời gian chạy game ở chế độ train, xong thời gian tự động dừng """
+ - time_run_game = Thời gian chạy game ở chế độ train, xong thời gian tự động dừng 
+ - [Train, Test] """
 
-type_run_code = 'Test' #Train or test
+type_run_code = 'Test' #Train or Test
 players = ['NhatAnh_0822', 'Trang']
 game_name = 'TLMN_v2'
 time_run_game = 1000
