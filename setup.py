cdf10d93ab9d93f9b9c5c43f63d3f3ecf5048d82
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-09-07 15:01:41 +0700

    add gitignore

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@
  - [Train, Test] """
 
 type_run_code = 'Test' #Train or Test
-players = ['LinhChi_0822']
-game_name = 'Splendor_OnlyPlayerView'
-time_run_game = 7200
+players = []
+game_name = 'SushiGo-main'
+time_run_game = 300
