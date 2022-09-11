122bc2b2220274261395c87a78cc275ecbdf48e2
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-09-11 16:57:50 +0700

    change game name

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
  - players: list player muốn truyền vào
     - ở chế độ Train thì sẽ train đa luồng tương ứng số người chơi
     - chế độ Test nếu truyền vào không đủ người chơi thì sẽ tự động thêm random vào
- - games_name = ['Splendor_OnlyPlayerView','CENTURY', 'MACHIKORO', 'SHERIFF', 'Splendor', 'TLMN', 'TLMN_v2', 'SushiGo-main']
+ - games_name = ['Splendor_v2','Century', 'MachiKoro', 'Sheriff', 'Splendor', 'TLMN', 'TLMN_v2', 'SushiGo']
  - game_name: Tên game, có thể lấy ở games_name
  - time_run_game = Thời gian chạy game ở chế độ train, xong thời gian tự động dừng 
  - [Train, Test] """
+
 
 type_run_code = 'Train' #Train or Test
 players = ['CatKhanh_0822_2']
