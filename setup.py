4bc2b5d9f46a976c3d0a6817f31591561bb2d33f
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-09-12 16:10:16 +0700

    fix type output game

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
  - players: list player muốn truyền vào
     - ở chế độ Train thì sẽ train đa luồng tương ứng số người chơi
     - chế độ Test nếu truyền vào không đủ người chơi thì sẽ tự động thêm random vào
- - games_name = ['Splendor_v2','Century', 'MachiKoro', 'Sheriff', 'Splendor', 'TLMN', 'TLMN_v2', 'SushiGo']
+ - games_name = ['Splendor_v2','Century', 'MachiKoro', 'Sheriff', 'Splendor', 'TLMN', 'TLMN_v2', 'SushiGo', 'Catan']
  - game_name: Tên game, có thể lấy ở games_name
  - time_run_game = Thời gian chạy game ở chế độ train, xong thời gian tự động dừng 
  - [Train, Test] """
