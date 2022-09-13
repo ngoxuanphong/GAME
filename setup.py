51e1cb5003e82b3b09f2ffb5d68ce1740e67ec88
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-09-14 01:18:26 +0700

    add tổ hợp test người chơi

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@
  - players: list player muốn truyền vào
     - ở chế độ Train thì sẽ train đa luồng tương ứng số người chơi
     - chế độ Test nếu truyền vào không đủ người chơi thì sẽ tự động thêm random vào
+    - Test nếu truyền vào nhiều hơn số người chơi của game đó thì chơi tổ hợp và trả ra file json
  - games_name = ['Splendor_v2','Century', 'MachiKoro', 'Sheriff', 'Splendor', 'TLMN', 'TLMN_v2', 'SushiGo', 'Catan']
  - game_name: Tên game, có thể lấy ở games_name
  - time_run_game = Thời gian chạy game ở chế độ train, xong thời gian tự động dừng 
  - [Train, Test]
  - 'Train_1_player' để chạy mỗi người chơi, Không chạy đa luồng, dùng để test khi để code vào hệ thống 
+
+ - Nâng cao dành cho người quản lý hệ thống:
+      - path_save_json_test_player để lưu file json của khi test tổ hợp các player
 """
 
 
 
 type_run_code = 'Test' #Train or Test or Train_1_player
-players = []
-game_name = 'SushiGo'
-time_run_game = 15000
+players = ['NhatAnh_New', 'NhatAnh_New1', 'NhatAnh_New2', 'NhatAnh_New3', 'NhatAnh_New4',]
+game_name = 'TLMN'
+time_run_game = 79200
+
+path_save_json_test_player = '' #Nơi lưu file json data test các người chơi
